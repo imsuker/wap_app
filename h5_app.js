e4d76fc07223cc6320ac0dd986a5859913c71c2b
imsuker <imsuker@qq.com>
2016-03-31 11:42:25 +0800

    wap_app for mobile wap1.0

---
+++ h5_app.js
@@ -0,0 +1,68 @@
+$(function(){
+	bridgeReady();
+	function bridgeReady(){
+    hideTopNavigate();//隐藏顶导航
+    observeA(); //对所有A链接的点击监听并通知native
+    hideHomeBottom();//隐藏首页底部的“关于老虎金融”和"合作伙伴"模块
+    hideTradeTopBottom();//隐藏交易页顶部信息和底部入金出金
+  }
+  //隐藏交易页顶部信息和底部入金出金
+  function hideTradeTopBottom(){
+    $(".asset-detail").livequery(function(){
+      $(this).remove();
+    });
+    $(".bt-button").livequery(function(){
+      $(this).remove();
+    });
+  }
+  //隐藏首页底部的“关于老虎金融”和"合作伙伴"模块
+  function hideHomeBottom(){
+    $(".aboutus").livequery(function(){
+      $(this).parents('.home__wrap_bg').remove();
+    });
+    $(".footer").livequery(function(){
+      $(this).parent().remove();
+    });
+  }
+  //隐藏顶导航
+  function hideTopNavigate(){
+		$(".navbar").livequery(function(){
+			$(this).hide();
+		});
+		$("body").css('margin-top','-49px');
+		$(".return_header").livequery(function(){
+			$(this).hide();
+		});
+  }
+  //对a链接的点击进行监听，阻止并通知native
+  function observeA(){
+		$("a").livequery(function(){
+			$(this).click(function(){
+				var href = $(this).attr('href');
+				console.log("a is clicked,and it's href is" + href);
+				if(!/^#.*/.test(href)){
+					return;
+				}
+				var url =location.href.replace(/#.*/g,href);
+				var oAction = {
+					"type" : "openUrl",
+					"url" : url
+				};
+				callNative(oAction);
+				return false;
+			});
+		});
+	}
+	function callNative(oAction){
+		var sAction = JSON.stringify(oAction);
+		if(isIOS()){
+			TigerwitNative(sAction);
+		}else{
+			TigerwitNative.jsCall(sAction);
+		}
+	}
+	function isIOS(){
+		return /like Mac OS X/.test(navigator.userAgent);
+	}
+
+});
